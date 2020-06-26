3cf81baf07e4ffcef3ff90e08055d4cdac434f4e
tom3108 <t.madziara@gmail.com>
2020-06-26 13:44:49 +0200

    Method Create Update and Delete

--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from api.serializers import UserSerializer
 from .models import Movie
 from .serializers import MovieSerializer, MovieMiniSerializer
+from django.http.response import HttpResponseNotAllowed
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -16,11 +18,35 @@ class MovieViewSet(viewsets.ModelViewSet):
         return qs
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        serializer = MovieMiniSerializer(queryset, many=True)
+        serializer = MovieSerializer(queryset, many=True)
         return Response(serializer.data)
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = MovieSerializer(instance)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        #if request.user.is_staff:
+        movie = Movie.objects.create(title=request.data['title'],
+                                         content=request.data['content'],
+                                         after_prem=request.data['after_prem'])
+        serializer = MovieSerializer(movie, many=False)
+        return Response(serializer.data)
+        #else:
+        #return HttpResponseNotAllowed('Not allowed')
+    def update(self, request, *args, **kwargs):
+        movie = self.get_object()
+        movie.title = request.data['title']
+        movie.content = request.data['content']
+        movie.after_prem = request.data['after_prem']
+        movie.save()
+        serializer = MovieSerializer(movie, many=False)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        movie = self.get_object()
+        movie.delete()
+        return Response('Deleted movie')
+
 
 
